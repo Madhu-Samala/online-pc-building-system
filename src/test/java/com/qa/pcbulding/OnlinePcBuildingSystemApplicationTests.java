deb95c0021c7f37f41709feabbd7512df4141582
MadhuSamala <madhusamala.trainer@gmail.com>
2022-10-26 10:34:36 +0100

    initial commit

---
+++ src/test/java/com/qa/pcbulding/OnlinePcBuildingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.pcbulding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePcBuildingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
